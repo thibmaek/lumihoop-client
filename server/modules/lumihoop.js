efb1332d25fa2386fc216284bb701e9d1e241358
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-06 19:53:04 +0100

    :fire: Remove useless user logs

--- server/modules/lumihoop.js
+++ server/modules/lumihoop.js
@@ -1,30 +1,14 @@
 module.exports.register = (server, options, next) => {
-
   const io = require(`socket.io`)(server.listener);
 
-  const users = [];
-
   io.on(`connection`, socket => {
-
-    const { id: socketId } = socket;
-    console.log(socketId);
-
-    const user = {
-      socketId,
-    };
-
-    users.push(user);
-    console.log(users);
-
     socket.on(`hoopPlaced`, ({ pageX, pageY, scale }) => {
       io.emit(`drawHoop`, { pageX, pageY, scale });
       console.log({ pageX, pageY, scale });
     });
-
   });
 
   next();
-
 };
 
 module.exports.register.attributes = {
