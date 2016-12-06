69e20ffe5b534d82b359b2f38633070ffb0b7908
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-06 21:11:55 +0100

    :art: Put variables in config.js, limit min scale of hoop, lock:onHoopPlaced

--- src/js/script.js
+++ src/js/script.js
@@ -1,39 +1,49 @@
 import io from 'socket.io-client';
+import { colors, shape, minScale, scaleMultiplier } from './config.js';
 
 const socket = io(`/`);
 
 const init = () => {
-
-  document.addEventListener(`touchmove`, e => {
-    e.preventDefault();
-  });
-
   const canvas = document.querySelector(`.canvas`);
+  canvas.setAttribute(`width`, window.innerWidth);
+  canvas.setAttribute(`height`, window.innerHeight);
+  let canPlaceHoop = true;
 
-  canvas.addEventListener(`gesturechange`, e => {
-    const { pageX, pageY, scale } = e;
+  document.addEventListener(`touchmove`, e => e.preventDefault());
+
+  const drawHoop = ({ pageX, pageY, scale }) => {
     const ctx = canvas.getContext(`2d`);
-
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fill = `black`;
+    ctx.fill = colors.bg;
     ctx.beginPath();
-    ctx.arc(pageX, pageY, scale * 100, 0, Math.PI * 2, false);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = `#ff69b4`;
+    ctx.arc(pageX, pageY, scale * scaleMultiplier, 0, Math.PI * 2, false);
+    ctx.lineWidth = shape.stroke;
+    ctx.strokeStyle = colors.pink;
     ctx.stroke();
 
-    console.log(pageX, pageY);
+    ctx.shadowBlur = shape.glow;
+    ctx.shadowColor = colors.pink;
+  };
+
+  canvas.addEventListener(`gesturechange`, e => {
+    if (e.scale > minScale && canPlaceHoop) drawHoop(e);
     requestAnimationFrame();
   });
 
   canvas.addEventListener(`gestureend`, e => {
     const { pageX, pageY, scale } = e;
-
     socket.emit(`hoopPlaced`, { pageX, pageY, scale });
+
+    const alert = document.querySelector(`.circle-is-placed`);
+    alert.classList.toggle(`is-hidden`);
+
+    canPlaceHoop = false;
+    setTimeout(() => {
+      canPlaceHoop = true;
+      alert.classList.toggle(`is-hidden`);
+    }, 10000);
   });
 
 };
-
-
 
 init();
