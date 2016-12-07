45783523982636c8390d8f73409576a525f8edc0
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-07 16:14:48 +0100

    :hammer: Move functions outside initialiser

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,51 @@
 import io from 'socket.io-client';
 import { colors, shape, minScale, scaleMultiplier } from './config.js';
 
+const $canvas = document.querySelector(`.canvas`);
+const $alert = document.querySelector(`.circle-is-placed`);
+
 const socket = io(`/`);
+let canPlaceHoop = true;
+
+const handleWSNewTurn = () => {
+  canPlaceHoop = true;
+  $alert.classList.toggle(`is-hidden`);
+};
+
+const drawHoop = ({ pageX, pageY, scale }) => {
+  const ctx = $canvas.getContext(`2d`);
+  ctx.fillRect(0, 0, $canvas.width, $canvas.height);
+  ctx.fill = colors.bg;
+  ctx.beginPath();
+  ctx.arc(pageX, pageY, scale * scaleMultiplier, 0, Math.PI * 2, false);
+  ctx.lineWidth = shape.stroke;
+  ctx.strokeStyle = colors.pink;
+  ctx.stroke();
+
+  ctx.shadowBlur = shape.glow;
+  ctx.shadowColor = colors.pink;
+};
 
 const init = () => {
-  //wacht op socket - bal in hoop
-  socket.on(`newTurn`, handleWSNewTurn);
-
-  const canvas = document.querySelector(`.canvas`);
-  canvas.setAttribute(`width`, window.innerWidth);
-  canvas.setAttribute(`height`, window.innerHeight);
-  let canPlaceHoop = true;
+  $canvas.setAttribute(`width`, window.innerWidth);
+  $canvas.setAttribute(`height`, window.innerHeight);
 
   document.addEventListener(`touchmove`, e => e.preventDefault());
 
-  const drawHoop = ({ pageX, pageY, scale }) => {
-    const ctx = canvas.getContext(`2d`);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fill = colors.bg;
-    ctx.beginPath();
-    ctx.arc(pageX, pageY, scale * scaleMultiplier, 0, Math.PI * 2, false);
-    ctx.lineWidth = shape.stroke;
-    ctx.strokeStyle = colors.pink;
-    ctx.stroke();
-
-    ctx.shadowBlur = shape.glow;
-    ctx.shadowColor = colors.pink;
-  };
-
-  canvas.addEventListener(`gesturechange`, e => {
+  $canvas.addEventListener(`gesturechange`, e => {
     if (e.scale > minScale && canPlaceHoop) drawHoop(e);
     requestAnimationFrame();
   });
 
-  canvas.addEventListener(`gestureend`, e => {
+  $canvas.addEventListener(`gestureend`, e => {
     const { pageX, pageY, scale } = e;
     socket.emit(`hoopPlaced`, { pageX, pageY, scale });
 
-    const alert = document.querySelector(`.circle-is-placed`);
-    alert.classList.toggle(`is-hidden`);
-
+    $alert.classList.toggle(`is-hidden`);
     canPlaceHoop = false;
   });
 
-  //start nieuwe beurt
-  const handleWSNewTurn = () => {
-    canPlaceHoop = true;
-    alert.classList.toggle(`is-hidden`);
-  };
-
+  socket.on(`newTurn`, handleWSNewTurn);
 };
 
 init();
