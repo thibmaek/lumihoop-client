dcff2408b7cce9501dcba86d92dafe45eb60823c
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-20 23:46:18 +0100

    :sparkles: Don't activate circle in wrong case Will not emit a socket event and dim the circle when it's too small or out of bounds

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 import io from 'socket.io-client';
 const socket = io(`/`);
 
-import { colors, shape, minScale, scaleMultiplier } from './config.js';
+import {
+  colors, shape, minScale, scaleMultiplier,
+  $canvas, $alert, $notificationBounds, $notificationScale,
+} from './config.js';
 
-const $canvas = document.querySelector(`.canvas`);
-const $alert = document.querySelector(`.circle-is-placed`);
 const ctx = $canvas.getContext(`2d`);
 
 let canPlaceHoop = true;
@@ -23,15 +24,28 @@ const drawHoop = ({ pageX, pageY, scale }) => {
   ctx.beginPath();
   ctx.arc(pageX, pageY, naturalScale, 0, Math.PI * 2, false);
   ctx.lineWidth = shape.stroke;
-  ctx.strokeStyle = colors.pink;
   ctx.stroke();
-
-  ctx.shadowBlur = shape.glow;
-  ctx.shadowColor = colors.pink;
 
   if (pageX - naturalScale < 0 || pageX + naturalScale > window.innerWidth ||
     pageY - naturalScale < 0 || pageY + naturalScale > window.innerHeight) {
-    // stop movement & scaling
+    $notificationBounds.classList.remove(`is-hidden`);
+    canPlaceHoop = false;
+    ctx.strokeStyle = colors.bg;
+    ctx.shadowBlur = `none`;
+  } else {
+    $notificationBounds.classList.add(`is-hidden`);
+    $notificationScale.classList.add(`is-hidden`);
+    canPlaceHoop = true;
+    ctx.strokeStyle = colors.pink;
+    ctx.shadowBlur = shape.glow;
+    ctx.shadowColor = colors.pink;
+  }
+
+  if (scale < minScale) {
+    $notificationScale.classList.remove(`is-hidden`);
+    canPlaceHoop = false;
+    ctx.strokeStyle = colors.bg;
+    ctx.shadowBlur = `none`;
   }
 };
 
@@ -42,7 +56,7 @@ const init = () => {
   document.addEventListener(`touchmove`, e => e.preventDefault());
 
   $canvas.addEventListener(`gesturechange`, e => {
-    if (e.scale > minScale && canPlaceHoop) drawHoop(e);
+    drawHoop(e);
     requestAnimationFrame();
   });
 
