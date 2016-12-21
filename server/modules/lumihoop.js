65f079de5bd1677db7d8dbe89225fe350c94b545
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-21 15:23:54 +0100

    :art: Emit clients on change

--- server/modules/lumihoop.js
+++ server/modules/lumihoop.js
@@ -1,7 +1,12 @@
 module.exports.register = (server, options, next) => {
   const io = require(`socket.io`)(server.listener);
+  let clients = [];
 
   io.on(`connection`, socket => {
+    const client = { id: socket.id };
+    clients.push(client);
+    socket.emit(`clientsChanged`, clients);
+
     socket.on(`hoopPlaced`, ({ relX, relY, scale }) => {
       io.emit(`drawHoop`, { relX, relY, scale });
       console.log({ relX, relY, scale });
@@ -10,6 +15,11 @@ module.exports.register = (server, options, next) => {
     socket.on(`detectHit`, () => io.emit(`newTurn`));
 
     socket.on(`debug`, data => console.log(data));
+  });
+
+  io.on(`disconnect`, socket => {
+    clients = clients.filter(c => c.id !== socket.id);
+    socket.emit(`clientsChanged`, clients);
   });
 
   next();
