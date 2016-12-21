06e575ef37af5c6c6717ca977e013885f2e769bd
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2016-12-21 11:33:56 +0100

    :bug: Fixed possibility to redraw after placing hoop

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,11 @@ import {
 
 const ctx = $canvas.getContext(`2d`);
 
-let canPlaceHoop = true;
+let stopEmit = false;
+let canDraw = true;
 
 const newTurn = () => {
-  canPlaceHoop = true;
+  canDraw = true;
   $alert.classList.add(`is-hidden`);
   ctx.fillRect(0, 0, $canvas.width, $canvas.height);
 };
@@ -28,14 +29,14 @@ const drawHoop = ({ pageX, pageY, scale }) => {
 
   if (pageX - naturalScale < 0 || pageX + naturalScale > window.innerWidth ||
     pageY - naturalScale < 0 || pageY + naturalScale > window.innerHeight) {
+    stopEmit = true;
     $notificationBounds.classList.remove(`is-hidden`);
-    canPlaceHoop = false;
     ctx.strokeStyle = colors.bg;
     ctx.shadowBlur = `none`;
   } else {
+    stopEmit = false;
     $notificationBounds.classList.add(`is-hidden`);
     $notificationScale.classList.add(`is-hidden`);
-    canPlaceHoop = true;
     ctx.strokeStyle = colors.pink;
     ctx.shadowBlur = shape.glow;
     ctx.shadowColor = colors.pink;
@@ -43,7 +44,6 @@ const drawHoop = ({ pageX, pageY, scale }) => {
 
   if (scale < minScale) {
     $notificationScale.classList.remove(`is-hidden`);
-    canPlaceHoop = false;
     ctx.strokeStyle = colors.bg;
     ctx.shadowBlur = `none`;
   }
@@ -56,19 +56,19 @@ const init = () => {
   document.addEventListener(`touchmove`, e => e.preventDefault());
 
   $canvas.addEventListener(`gesturechange`, e => {
-    drawHoop(e);
+    if (canDraw) drawHoop(e);
     requestAnimationFrame();
   });
 
   $canvas.addEventListener(`gestureend`, e => {
-    if (canPlaceHoop) {
+    if (!stopEmit) {
       const { pageX, pageY, scale } = e;
       const relX = (pageX - (window.innerWidth / 2)) / (window.innerWidth / 2);
       const relY = (pageY - (window.innerHeight / 2)) / (window.innerHeight / 2);
       socket.emit(`hoopPlaced`, { relX, relY, scale });
 
+      canDraw = false;
       $alert.classList.remove(`is-hidden`);
-      canPlaceHoop = false;
     }
   });
 
