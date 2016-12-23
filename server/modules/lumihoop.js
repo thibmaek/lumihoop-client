2e62c3c8c1645d1b0222be0131445fb54ff070c1
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2016-12-23 11:54:09 +0100

    Release (#25)
    * :art: Emit clients on change
    
    * :art: Add option to send clients
    
    * :bug: fuck this
    
    * :pencil2: lol
    
    * :rocket: Add runable command

--- server/modules/lumihoop.js
+++ server/modules/lumihoop.js
@@ -1,13 +1,23 @@
 module.exports.register = (server, options, next) => {
   const io = require(`socket.io`)(server.listener);
 
+  let clients = 0;
+
   io.on(`connection`, socket => {
+    socket.emit(`clientsChanged`, { clients: clients += 1 });
+    console.log(clients);
+
     socket.on(`hoopPlaced`, ({ relX, relY, scale }) => {
       io.emit(`drawHoop`, { relX, relY, scale });
       console.log({ relX, relY, scale });
     });
 
     socket.on(`detectHit`, () => io.emit(`newTurn`));
+
+    socket.on(`disconnect`, () => {
+      io.emit(`clientsChanged`, { clients: clients -= 1 });
+      console.log(clients);
+    });
 
     socket.on(`debug`, data => console.log(data));
   });
